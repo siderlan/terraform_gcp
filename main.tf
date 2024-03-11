d58986615e00352f7355f8a9be487edcb1a8a047
Siderlan Santos <siderlan.santos@ferreri.co>
2024-03-11 16:46:33 -0300

    feat: add website

--- main.tf
+++ main.tf
@@ -26,18 +26,6 @@ module "app_network" {
   ]
 }
 
-resource "google_compute_network" "app" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "app" {
-  name          = var.network_name
-  ip_cidr_range = var.network_ip_range
-  region        = var.region
-  network       = google_compute_network.app.id
-}
-
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = var.image_project
@@ -45,8 +33,10 @@ data "google_compute_image" "ubuntu" {
 }
 
 resource "google_compute_instance" "blog" {
-  name = var.app_name
+  name         = var.app_name
   machine_type = var.machine_type
+
+  tags         = ["${var.network_name}-web"]
 
   boot_disk {
     initialize_params {
@@ -59,6 +49,8 @@ resource "google_compute_instance" "blog" {
       # Leave empty for dynamic public IP
     }
   }
+
+  metadata_startup_script = "apt -y update; apt -y install nginx; echo ${var.app_name} > /var/www/html/index.html"
 
   allow_stopping_for_update = true 
   
